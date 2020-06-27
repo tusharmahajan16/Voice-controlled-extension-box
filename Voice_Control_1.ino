024da95f40b00ebaa9809539450c740c40c973a1
Tushar Mahajan <41909171+tusharmahajan16@users.noreply.github.com>
2020-06-27 08:49:24 +0530

    Add files via upload

---
+++ Voice_Control_1.ino
@@ -0,0 +1,45 @@
+#include <SoftwareSerial.h>
+String value;
+
+void setup() {
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  Serial.begin(9600);       // start serial communication at 9600bps
+  bluetooth.begin(9600);
+}
+
+void loop() {
+  Serial.println(value);
+ if (bluetooth.available())
+   {
+    value = bluetooth.readString();
+
+    if (value == "All Switches On"){
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);  
+      }
+
+    if (value == "All Awitches Off"){
+      digitalWrite(9, LOW); 
+      digitalWrite(10, LOW);       
+      }
+
+    if (value == "Switch 1 On"){
+    digitalWrite(9, HIGH); 
+      }
+
+    if (value == "Switch 2 On"){
+      digitalWrite(10, HIGH);       
+      }
+      
+    if (value == "switch 1 Off"){
+    digitalWrite(9, LOW); 
+      }
+
+    if (value == "Switch 2 Off"){
+      digitalWrite(10, LOW);       
+      }
+
+ }
+
+}
---
+++ Voice_Controlled_Extension_Box.ino
@@ -0,0 +1,64 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BT(0, 1); //TX, RX respetively
+String readvoice;
+
+void setup() {
+ BT.begin(9600);
+ Serial.begin(9600);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+
+}
+//-----------------------------------------------------------------------// 
+void loop() {
+  while (BT.available()){  //Check if there is an available byte to read
+  delay(10); //Delay added to make thing stable
+  char c = BT.read(); //Conduct a serial read
+  readvoice += c; 
+  } 
+  if (readvoice.length() > 0) {
+    Serial.println(readvoice);
+
+  if(readvoice == "both switch on")
+  {
+    digitalWrite(3, LOW);
+    digitalWrite (4, LOW);
+ 
+    delay(100);
+  }
+
+  else if(readvoice == "both switch off")
+  {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+
+    delay(100);
+  }
+
+  else if (readvoice == "first switch on")
+  {
+    digitalWrite (3, LOW);
+   delay (100);
+  
+  }
+
+ else if ( readvoice == "second switch on")
+ {
+   digitalWrite (4, LOW);
+    delay(100);
+ }
+
+ else if (readvoice == "first switch off")
+ {
+   digitalWrite (3, HIGH );
+   delay (100);
+ }
+ else if (readvoice == "second switch off")
+ {
+   digitalWrite (4, HIGH);
+   delay (100);
+ }
+  
+
+ readvoice="";}} //Reset the variable
